3891f4273a381a56783b7afe445807b9b8d6dd9f
Wesleyoabrs <wesley.replit1994@gmail.com>
2026-08-05 17:45:01 -0300

    Update app.ts
    SEGUNDA ALTERAÇÃO PARA O RENDER

--- artifacts/api-server/src/app.ts
+++ artifacts/api-server/src/app.ts
@@ -10,7 +10,7 @@ app.use((req: any, res: any, next) => {
   next();
 });
 
-app.use(cors());
+app.use(cors({   origin: "*",   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],   allowedHeaders: ["Content-Type", "Authorization"] }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
