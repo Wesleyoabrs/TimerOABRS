6fedf4e24e350302b3bec02690c48b7df25a9d44
Wesleyoabrs <wesley.replit1994@gmail.com>
2026-08-05 17:15:33 -0300

    Update app.ts
    RENDER ALTERAÇÃO

--- artifacts/api-server/src/app.ts
+++ artifacts/api-server/src/app.ts
@@ -1,30 +1,15 @@
 import express, { type Express } from "express";
 import cors from "cors";
-import * as pinoHttp from 'pino-http';
 import router from "./routes";
-import { logger } from "./lib/logger";
 
 const app: Express = express();
 
-app.use(
-  pinoHttp({
-    logger,
-    serializers: {
-      req(req) {
-        return {
-          id: req.id,
-          method: req.method,
-          url: req.url?.split("?")[0],
-        };
-      },
-      res(res) {
-        return {
-          statusCode: res.statusCode,
-        };
-      },
-    },
-  }),
-);
+// Middleware simples para substituir o pinoHttp problemático nos logs
+app.use((req: any, res: any, next) => {
+  console.log(`[${req.method}] ${req.url}`);
+  next();
+});
+
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
